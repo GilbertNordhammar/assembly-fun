2b79c3c385c621a655d618439dddb0482481784b
Gilbert Nordhammar <gilbertnordhammar@gmail.com>
2023-04-24 11:41:44 +0200

    Set up snake project

---
+++ Examples/Snake/src/Foo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+extern "C" int __cdecl GetSum(int a, int b, int c, int d, int e)
+{
+    return a + b + c + d + e;
+}
+
+extern "C" void __cdecl HelloWorldCStyle()
+{
+    std::cout << "Hello World from outside ASM with C-style!" << std::endl;
+}
+
+void __cdecl HelloWorldCPlusPlusStyle()
+{
+    std::cout << "Hello World from outside ASM C++ style!" << std::endl;
+}
+
+namespace SomeNamespace
+{
+    void Foo()
+    {
+        std::cout << "SomeNamespace::Foo()" << std::endl;
+    }
+}
